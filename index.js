d114c0d55f30d09958057dbf846aee8ea1a6471c
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-09 15:53:53 +0300

    commit tree b8e24278b3395f12266c76e48caa58043e0d4bdd

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(anArray) {
+    return anArray.slice(0, 2);
+}
+
+function returnLastTwoDrivers(anArray) {
+    return anArray.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiplier) {
+    return (fare) => fare * multiplier;
+}
+
+
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, functionName) {
+    return functionName(arrayOfDrivers);
+}
